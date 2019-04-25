0e8e9a8e3fa1fa497c75f794fa78528042072220
semicon <30399464+semicon@users.noreply.github.com>
2019-04-25 12:00:37 +0700

    spelling
    spelling

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1556166234,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/boxstand-sheet0.png",
+		"images/animalmv-sheet0.png",
+		"images/boxrandom-sheet0.png",
+		"images/particles.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
